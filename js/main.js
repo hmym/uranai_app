14379fdabc173704f1385beabe580f460a8a115b
hmym <chika.hamayama@gmail.com>
2019-03-23 15:23:38 +0900

    btn_Math.random

--- js/main.js
+++ js/main.js
@@ -4,7 +4,21 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!';
+    const results = ['bad', 'soso', 'good!', 'crazy!'];
+    const n = Math.floor(Math.random() * results.length);
+    btn.textContent = results[n];
+    // btn.textContent = n;
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = "bad"
+    //     break;
+    //   case 1:
+    //     btn.textContent = "soso"
+    //     break;
+    //   case 2:
+    //     btn.textContent = "good!"
+    //     break;
+    // }
   });
 
   btn.addEventListener('mousedown', () => {
