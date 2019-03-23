9bdca6a2e274011cc2f9715ace2900a8791cf10e
hmym <chika.hamayama@gmail.com>
2019-03-23 16:04:09 +0900

    add if_msg

--- js/main.js
+++ js/main.js
@@ -2,23 +2,19 @@
 
 {
   const btn = document.getElementById('btn');
+  const msg = document.getElementById('msg');
 
   btn.addEventListener('click', () => {
     const results = ['bad', 'soso', 'good!', 'crazy!'];
     const n = Math.floor(Math.random() * results.length);
     btn.textContent = results[n];
-    // btn.textContent = n;
-    // switch (n) {
-    //   case 0:
-    //     btn.textContent = "bad"
-    //     break;
-    //   case 1:
-    //     btn.textContent = "soso"
-    //     break;
-    //   case 2:
-    //     btn.textContent = "good!"
-    //     break;
-    // }
+    if (n === 0) {
+      msg.textContent = "Don't worry!";
+    }else if ( n === 3){
+      msg.textContent = "Let's calm down!";
+    }else{
+      msg.textContent = "Good Luck!";
+    }
   });
 
   btn.addEventListener('mousedown', () => {
