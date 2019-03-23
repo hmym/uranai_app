559ab65b9bd1cdefc3ffbc6f699d535ef0126bf3
hmym <chika.hamayama@gmail.com>
2019-03-23 17:25:55 +0900

    add canvas

--- js/main.js
+++ js/main.js
@@ -25,4 +25,35 @@
     btn.classList.remove('pressed');
   });
 
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  const context = document.getElementById("canvas").getContext("2d");
+
+  function drowLine(startX, startY, endX, endY, color1, color2, color3) {
+    context.globalCompositeOperation = 'lighter';
+    context.strokeStyle = "rgba(" + color1 +", " + color2 + ", " + color3 + ", 1)";
+    context.lineWidth = 1;
+    context.beginPath();
+    context.moveTo(startX, startY);
+    context.lineTo(endX, endY);
+    context.stroke();
+  }
+
+  function call() {
+    context.clearRect(0, 0, 300, 200);
+    for (let i = 0; i < 100; i ++) {
+      drowLine(
+        getRandomInt(100),
+        getRandomInt(100),
+        getRandomInt(300),
+        getRandomInt(300),
+        255 - getRandomInt(180),
+        255 - getRandomInt(255),
+        255 - getRandomInt(255)
+      );
+    }
+  }
+  setInterval(call, 33);
 }
